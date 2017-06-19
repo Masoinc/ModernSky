e4294c8187e5eee74d804f98ad1e8bff3a92468f
Masonic <Masonic9@live.com>
2017-06-19 12:25:39 +0800

    update

--- src/me/masonic/mc/Function/Vip.java
+++ src/me/masonic/mc/Function/Vip.java
@@ -1,11 +1,31 @@
 package me.masonic.mc.Function;
 
+import me.masonic.mc.Utility.SqlUtility;
+import org.bukkit.entity.Player;
+import ru.tehkode.permissions.bukkit.PermissionsEx;
+
+import java.util.HashMap;
+
 /**
  * Mason Project
  * 2017-6-17-0017
  */
 public class Vip {
+    private static HashMap<String, Integer> VIPMAP = new HashMap<>();
+    static {
+        VIPMAP.put("default", 0);
+        VIPMAP.put("VIP", 1);
+        VIPMAP.put("VIP+", 2);
+        VIPMAP.put("SVIP", 3);
+    }
 
-    
-
+    public static String getVipRank(Player p) {
+        String res = "default";
+        for (String group : VIPMAP.keySet()) {
+            if (PermissionsEx.getUser(p).inGroup(group) && VIPMAP.get(group) > VIPMAP.get(res)) {
+                res = group;
+            }
+        }
+        return res;
+    }
 }
