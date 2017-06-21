26d74d4cc09989ed935482e56d5d65c729a38a9a
Masonic <Masonic9@live.com>
2017-06-21 12:39:01 +0800

    update

--- src/me/masonic/mc/Cmd/MskyVip.java
+++ src/me/masonic/mc/Cmd/MskyVip.java
@@ -28,52 +28,216 @@ public class MskyVip implements CommandExecutor {
                 case 3:
                     int days = Integer.parseInt(args[2]);
                     int kita = days / 15;
-                    Player pr;
+                    Player pr = Bukkit.getPlayer(args[1]);
                     switch (args[0]) {
                         case "vip":
-                            pr = Bukkit.getPlayer(args[1]);
-                            if (Vip.getVipRank(pr).equals("SVIP") || Vip.getVipRank(pr).equals("SVIP+")) {
-                                pr.sendMessage("§8[ §6ModernSky §8] §7已开通Svip或Svip+");
-                                p.sendMessage("§8[ §6ModernSky §8] §7已开通Svip或Svip+");
-                                return true;
-                            }
-                            try {
-                                if (SqlUtility.getIfExist(pr, "vip")) {
-                                    // 续费
-                                    if (SqlUtility.getIntValue(p, "vip", "expiration") != 0 &&
-                                            SqlUtility.getIntValue(p, "vip", "expiration") > (System.currentTimeMillis() / 1000)) {
+                            switch (Vip.getVipRank(pr)) {
+                                case "SVIP+":
+                                    pr.sendMessage("§8[ §6ModernSky §8] §7已开通Svip或Svip+");
+                                    p.sendMessage("§8[ §6ModernSky §8] §7已开通Svip或Svip+");
+                                    return true;
+                                case "SVIP":
+                                    pr.sendMessage("§8[ §6ModernSky §8] §7已开通Svip或Svip+");
+                                    p.sendMessage("§8[ §6ModernSky §8] §7已开通Svip或Svip+");
+                                    return true;
+                                case "VIP":
+                                    try {
+                                        if (!SqlUtility.getIfExist(pr, "vip")) {
+                                            SqlUtility.createColumn(pr,"vip");
+                                        }
+                                        // VIP -> VIP
+                                        if (SqlUtility.getIntValue(p, "vip", "expiration") != 0 &&
+                                                SqlUtility.getIntValue(p, "vip", "expiration") > (System.currentTimeMillis() / 1000)) {
 
-                                        Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "pex user " + pr.getName() + " group add vip");
-                                        Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "cr give " + pr.getName() + " kitvipv1 " + kita);
+                                            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "pex user " + pr.getName() + " group add vip");
+                                            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "cr give " + pr.getName() + " kitvipv1 " + kita);
 
 
-                                        pr.sendMessage("§8[ §6ModernSky §8] §7已续费§2 Vip §7感谢您的支持");
+                                            pr.sendMessage("§8[ §6ModernSky §8] §7已续费§2 Vip §7，§7感谢您的支持");
 
-                                        SqlUtility.uploadIntValue(pr, "vip", "expiration", (SqlUtility.getIntValue(p, "vip", "expiration") + 86400 * days));
+                                            SqlUtility.uploadIntValue(pr, "vip", "expiration", (SqlUtility.getIntValue(p, "vip", "expiration") + 86400 * days));
+                                            return true;
+                                        }
+                                    } catch (SQLException e) {
+                                        e.printStackTrace();
                                     }
-                                }
-                            } catch (SQLException e) {
-                                e.printStackTrace();
+                                    return true;
+                                //DEFAULT -> VIP
+                                default:
+
+                                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "pex user " + pr.getName() + " group add vip");
+                                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "cr give " + pr.getName() + " kitvipv1 " + kita);
+
+                                    pr.sendMessage("§8[ §6ModernSky §8] §7已开通§2 Vip ，§7感谢您的支持");
+                                    try {
+                                        SqlUtility.createColumn(p, "vip");
+                                        SqlUtility.uploadIntValue(pr, "vip", "expiration", (int) ((System.currentTimeMillis() / 1000) + 86400 * days));
+                                    } catch (SQLException e) {
+                                        e.printStackTrace();
+                                    }
+                                    return true;
                             }
-                            return true;
+
                         case "svip":
-                            pr = Bukkit.getPlayer(args[1]);
-                            if ( Vip.getVipRank(pr).equals("SVIP+")) {
-                                pr.sendMessage("§8[ §6ModernSky §8] §7已开通Svip+");
-                                p.sendMessage("§8[ §6ModernSky §8] §7已开通Svip+");
-                                return true;
+                            switch (Vip.getVipRank(pr)) {
+                                // SVIP+
+                                case "SVIP+":
+                                    pr.sendMessage("§8[ §6ModernSky §8] §7已开通Svip+");
+                                    p.sendMessage("§8[ §6ModernSky §8] §7已开通Svip+");
+                                    return true;
+                                //SVIP -> SVIP
+                                case "SVIP":
+                                    try {
+                                        if (!SqlUtility.getIfExist(pr, "vip")) {
+                                            SqlUtility.createColumn(pr,"vip");
+                                        }
+                                        if (SqlUtility.getIntValue(p, "vip", "expiration") != 0 &&
+                                                SqlUtility.getIntValue(p, "vip", "expiration") > (System.currentTimeMillis() / 1000)) {
+
+                                            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "pex user " + pr.getName() + " group add svip");
+                                            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "cr give " + pr.getName() + " kitvipv2 " + kita);
+
+
+                                            pr.sendMessage("§8[ §6ModernSky §8] §7已续费§6 Svip §7，§7感谢您的支持");
+
+                                            SqlUtility.uploadIntValue(pr, "vip", "expiration", (SqlUtility.getIntValue(p, "vip", "expiration") + 86400 * days));
+                                            return true;
+                                        }
+                                    } catch (SQLException e) {
+                                        e.printStackTrace();
+                                    }
+                                    return true;
+                                //VIP -> SVIP
+                                case "VIP":
+                                    try {
+                                        if (!SqlUtility.getIfExist(pr, "vip")) {
+                                            SqlUtility.createColumn(pr,"vip");
+                                        }
+                                        if (SqlUtility.getIntValue(p, "vip", "expiration") != 0) {
+                                            int daysbonus = (int) (SqlUtility.getIntValue(p, "vip", "expiration") - (System.currentTimeMillis() / 1000)) / 2;
+
+                                            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "pex user " + pr.getName() + " group add svip");
+                                            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "cr give " + pr.getName() + " kitvipv2 " + kita);
+
+
+                                            pr.sendMessage("§8[ §6ModernSky §8] §7已由§2 Vip§7升级为§6Svip§7 ，§7感谢您的支持");
+
+                                            SqlUtility.uploadIntValue(pr, "vip", "expiration", (int) ((System.currentTimeMillis() / 1000) + 86400 * (days + daysbonus)));
+                                            return true;
+                                        }
+                                    } catch (SQLException e) {
+                                        e.printStackTrace();
+                                    }
+                                    return true;
+                                //DEFAULT -> SVIP
+                                default:
+
+                                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "pex user " + pr.getName() + " group add vip");
+                                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "cr give " + pr.getName() + " kitvipv1 " + kita);
+
+                                    pr.sendMessage("§8[ §6ModernSky §8] §7已开通§2 Vip ，§7感谢您的支持");
+                                    try {
+                                        SqlUtility.createColumn(p, "vip");
+                                        SqlUtility.uploadIntValue(pr, "vip", "expiration", (int) ((System.currentTimeMillis() / 1000) + 86400 * days));
+                                    } catch (SQLException e) {
+                                        e.printStackTrace();
+                                    }
+                                    return true;
+
                             }
                         case "svip+":
+                            switch (Vip.getVipRank(pr)) {
+                                case "VIP":
+                                    try {
+                                        if (!SqlUtility.getIfExist(pr, "vip")) {
+                                            SqlUtility.createColumn(pr,"vip");
+                                        }
+                                        if (SqlUtility.getIntValue(p, "vip", "expiration") != 0) {
+                                            int daysbonus = (int) (SqlUtility.getIntValue(p, "vip", "expiration") - (System.currentTimeMillis() / 1000)) / 3;
 
+                                            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "pex user " + pr.getName() + " group add svip+");
+                                            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "cr give " + pr.getName() + " kitvipv3 " + kita);
+
+
+                                            pr.sendMessage("§8[ §6ModernSky §8] §7已由§2 Vip§7升级为§cSvip+§7 ，§7感谢您的支持");
+
+                                            SqlUtility.uploadIntValue(pr, "vip", "expiration", (int) ((System.currentTimeMillis() / 1000) + 86400 * (days + daysbonus)));
+                                            return true;
+                                        }
+                                    } catch (SQLException e) {
+                                        e.printStackTrace();
+                                    }
+                                    return true;
+                                case "SVIP":
+                                    try {
+                                        if (!SqlUtility.getIfExist(pr, "vip")) {
+                                            SqlUtility.createColumn(pr,"vip");
+                                        }
+                                        if (SqlUtility.getIntValue(p, "vip", "expiration") != 0) {
+                                            int daysbonus = (int) (SqlUtility.getIntValue(p, "vip", "expiration") - (System.currentTimeMillis() / 1000)) / 2;
+
+                                            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "pex user " + pr.getName() + " group add svip+");
+                                            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "cr give " + pr.getName() + " kitvipv3 " + kita);
+
+
+                                            pr.sendMessage("§8[ §6ModernSky §8] §7已由§6 SVip§7升级为§cSvip+§7 ，§7感谢您的支持");
+
+                                            SqlUtility.uploadIntValue(pr, "vip", "expiration", (int) ((System.currentTimeMillis() / 1000) + 86400 * (days + daysbonus)));
+                                            return true;
+                                        }
+                                    } catch (SQLException e) {
+                                        e.printStackTrace();
+                                    }
+                                    return true;
+                                case "SVIP+":
+                                    try {
+                                        if (!SqlUtility.getIfExist(pr, "vip")) {
+                                            SqlUtility.createColumn(pr,"vip");
+                                        }
+                                        if (SqlUtility.getIntValue(p, "vip", "expiration") != 0 &&
+                                                SqlUtility.getIntValue(p, "vip", "expiration") > (System.currentTimeMillis() / 1000)) {
+
+                                            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "pex user " + pr.getName() + " group add svip+");
+                                            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "cr give " + pr.getName() + " kitvipv3 " + kita);
+
+
+                                            pr.sendMessage("§8[ §6ModernSky §8] §7已续费§c Svip+ §7，§7感谢您的支持");
+
+                                            SqlUtility.uploadIntValue(pr, "vip", "expiration", (SqlUtility.getIntValue(p, "vip", "expiration") + 86400 * days));
+                                            return true;
+                                        }
+                                    } catch (SQLException e) {
+                                        e.printStackTrace();
+                                    }
+                                    return true;
+                                default:
+                                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "pex user " + pr.getName() + " group add svip+");
+                                    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "cr give " + pr.getName() + " kitvipv3 " + kita);
+
+                                    pr.sendMessage("§8[ §6ModernSky §8] §7已开通§c Svip+ ，§7感谢您的支持");
+                                    try {
+                                        SqlUtility.createColumn(p, "vip");
+                                        SqlUtility.uploadIntValue(pr, "vip", "expiration", (int) ((System.currentTimeMillis() / 1000) + 86400 * days));
+                                    } catch (SQLException e) {
+                                        e.printStackTrace();
+                                    }
+                                    return true;
+
+
+                            }
                         default:
                             p.sendMessage("§8[ §6ModernSky §8] §7指令有误");
                             return true;
                     }
+                default:
+                    p.sendMessage("§8[ §6ModernSky §8] §7指令有误");
+                    return true;
             }
         } else {
-            commandSender.sendMessage("§8[ §ModernSky §8] §7权限不足");
+            commandSender.sendMessage("§8[ §6ModernSky §8] §7权限不足");
             return true;
         }
-        return false;
     }
+
+
 }
