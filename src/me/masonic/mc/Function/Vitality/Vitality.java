819a4263182bb9359b7821857d6e6115d8b0b3a8
Masonic <Masonic9@live.com>
2018-02-17 15:04:17 +0800

    Version 0.2.8 Date 18.2.17

--- src/me/masonic/mc/Function/Vitality/Vitality.java
+++ src/me/masonic/mc/Function/Vitality/Vitality.java
@@ -9,10 +9,12 @@ import me.masonic.mc.Utility.SqlUtil;
 import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.ChestMenu;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.ehcache.CacheManager;
@@ -68,7 +70,8 @@ public class Vitality implements Listener {
         menu.addMenuClickHandler(37, (p1, p2, p3, p4) -> false);
         int index = 0;
         for (VitalityQuest vq : VitalityQuest.values()) {
-            menu.addItem(quest_slot.get(index), vq.getIcon());
+            boolean completed = VitalityRecord.getInstance(p.getUniqueId()).getProgress(vq.codename) >= 100;
+            menu.addItem(quest_slot.get(index), vq.getIcon(completed));
             menu.addMenuClickHandler(quest_slot.get(index), (p1, p2, p3, p4) -> false);
             index++;
         }
@@ -103,7 +106,8 @@ class VitalityRecord {
         meta.setDisplayName("§8[ §6活跃度信息 §8]");
         ArrayList<String> lores = new ArrayList<>();
         lores.add("");
-        lores.add("§7◇ 目前活跃度: " + this.vitality);
+        lores.add("§7◇ 目前活跃度: §6" + this.vitality);
+        lores.add("§7△ 活跃度每日 §64:00 §7清零");
 //        lores.add("§7◇ 已完成的任务链:  " + this.desc);
         lores.add("");
         lores.add("§8[ ModernSky ] vitality");
@@ -206,52 +210,58 @@ class VitalityCache {
 }
 
 enum VitalityQuest {
-    LOGIN1("LOGIN1", "登入游戏", 5, new Reward(50, 0, new HashMap<String, Integer>() {{
+    LOGIN1("§7每日打卡 §8- §6I", "LOGIN1", "登入游戏", 5, new Reward(50, 0, new HashMap<String, Integer>() {{
     }})),
-    PLAY1("PLAY1", "在线 §61 §7小时", 10, new Reward(100, 20, new HashMap<String, Integer>() {{
+    PLAY1("§7每日打卡 §8- §6II", "PLAY1", "在线 §61 §7小时", 10, new Reward(100, 20, new HashMap<String, Integer>() {{
     }})),
-    PLAY2("PLAY2", "§611:00-13:00 §7期间在线", 5, new Reward(50, 20, new HashMap<String, Integer>() {{
+    PLAY2("§7每日打卡 §8- §6III", "PLAY2", "§611:00-13:00 §7期间在线", 5, new Reward(50, 20, new HashMap<String, Integer>() {{
     }})),
-    PLAY3("PLAY3", "§617:00-19:00 §7期间在线", 5, new Reward(100, 20, new HashMap<String, Integer>() {{
+    PLAY3("§7每日打卡 §8- §6IV", "PLAY3", "§617:00-19:00 §7期间在线", 5, new Reward(100, 20, new HashMap<String, Integer>() {{
     }})),
-    SLIMEFUN1("SLIMEFUN1", "制造任意一种太阳能发电机", 15, new Reward(120, 0, new HashMap<String, Integer>() {{
+    SLIMEFUN1("§7Technawlgy is pawer §8- §6I", "SLIMEFUN1", "制造任意一种太阳能发电机", 15, new Reward(120, 0, new HashMap<String, Integer>() {{
         put("sf3", 1);
     }})),
-    SLIMEFUN2("SLIMEFUN2", "制造任意一种蓄电池", 15, new Reward(120, 0, new HashMap<String, Integer>() {{
+    SLIMEFUN2("§7Technawlgy is pawer §8- §6II", "SLIMEFUN2", "制造任意一种蓄电池", 15, new Reward(120, 0, new HashMap<String, Integer>() {{
         put("sf13", 4);
     }})),
-    SLIMEFUN3("SLIMEFUN3", "制造 §9末影之尘 §8- §2III §7x3", 20, new Reward(80, 0, new HashMap<String, Integer>() {{
+    SLIMEFUN3("§7Magika is pawer §8- §6I", "SLIMEFUN3", "制造 §9末影之尘 §8- §2III §7x3", 20, new Reward(80, 0, new HashMap<String, Integer>() {{
         put("sf8", 10);
     }})),
-    ADVANCEDAB1("ADVANCEDAB1", "解锁任意被动天赋", 20, new Reward(150, 0, new HashMap<String, Integer>() {{
+    ADVANCEDAB1("§7天选之人 §8- §6I", "ADVANCEDAB1", "解锁任意被动天赋", 20, new Reward(150, 0, new HashMap<String, Integer>() {{
     }})),
-    KILL1("KILL1", "无伤击杀 §620 §7只僵尸", 15, new Reward(120, 0, new HashMap<String, Integer>() {{
+    KILL1("§7天选之人 §8- §6I", "无伤击杀 §620 §7只僵尸", "KILL1", 15, new Reward(120, 0, new HashMap<String, Integer>() {{
     }})),
-    POTION1("POTION1", "酿造 §63 §7种药水", 15, new Reward(60, 20, new HashMap<>()).appendRawItem(new HashMap<Material, Integer>() {{
+    POTION1("§7魔法师的必修课 §8- §6I", "POTION1", "酿造 §63 §7种药水", 15, new Reward(60, 20, new HashMap<>()).appendRawItem(new HashMap<Material, Integer>() {{
         put(Material.SAND, 10);
     }}));
 
     String desc;
     String codename;
+    String display;
     int vitality;
     Reward reward;
 
-    VitalityQuest(String codename, String desc, int vitality, Reward reward) {
+    VitalityQuest(String display, String codename, String desc, int vitality, Reward reward) {
+        this.display = display;
         this.desc = desc;
         this.codename = codename;
         this.vitality = vitality;
         this.reward = reward;
     }
 
-    ItemStack getIcon() {
-        ItemStack icon = new ItemStack(Material.BOOK);
+    ItemStack getIcon(boolean completed) {
+        ItemStack icon = new ItemStack(Material.BOOK_AND_QUILL);
         ItemMeta meta = icon.getItemMeta();
-        meta.setDisplayName(MessageFormat.format("§8[ §6活跃度任务·{0} §8]", this.codename));
+        if (completed) {
+            meta.addEnchant(Enchantment.LOOT_BONUS_MOBS, 1, true);
+            meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        }
+        meta.setDisplayName(MessageFormat.format("§8[ §6活跃度任务§8 · {0} §8]", completed ? "§7已完成" : this.display));
         ArrayList<String> lores = new ArrayList<>();
         lores.add("");
         lores.add("§7▽ 任务目标:");
-        lores.add("§7○ " + this.desc);
-        lores.add("");
+        lores.add("§7○ " + (completed ? "§m" : "") + this.desc);
+        lores.add("");//;
         lores.add("§7▽ 任务奖励: ");
         lores.addAll(this.reward.getLore());
         lores.add("§7○ §6活跃度§7 x " + String.valueOf(this.vitality));
